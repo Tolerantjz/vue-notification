202fb12112e5ace2f3e7e55fd7353f3d081d0786
Tolerantjz <1260285984@qq.com>
2017-03-21 12:19:43 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="app">
+  	<ul ref = "tit">
+  		<li v-for = "(v,index) in list" 
+  				@click = "handleClick(null,index)"
+  				ref = "content[index]">{{v.msg}}</li>
+  	</ul>
+  	<div>
+  		<ul>
+  			<li v-for = "(item,index) in infoItems" 
+  					v-model = "infoElement"
+  				  is = "info" 
+  					title = "Info" 
+  					content = "I am info"
+  					v-on:remove = "infoItems.splice(index,1)"></li>
+				<li v-for = "(item,index) in errorItems"
+					v-model = "errorElement"
+				  is = "wrong" 
+					title = "Error" 
+					content = "I am error"
+					v-on:remove = "errorItems.splice(index,1)"></li>
+				<li v-for = "(item,index) in successItems" 
+					v-model = "successElement"
+				  is = "success" 
+					title = "Success" 
+					content = "I am success"
+					v-on:remove = "successItems.splice(index,1)"></li>
+				<li v-for = "(item,index) in warnItems" 
+					v-model = "warnElement"
+				  is = "warn" 
+					title = "Warn" 
+					content = "I am warn"
+					v-on:remove = "warnItems.splice(index,1)"></li>
+  		</ul>
+  	</div>
+  </div>
+</template>
+
+<script>
+const list = [{msg:'Info'},{msg:'Error'},{msg:'success'},{msg:'warn'}]
+const errorItems = [{}]
+const successItems = [{}]
+const infoItems = [{}]
+const warnItems = [{}]
+import wrong from './components/error.vue'
+import info from './components/info.vue'
+import success from './components/success.vue'
+import warn from './components/warn.vue'
+export default {
+  name: 'app',
+  components:{
+  	wrong,
+  	info,
+  	success,
+  	warn
+  },
+  data () {
+    return {
+     	list,
+     	infoItems,
+     	errorItems,
+     	successItems,
+     	warnItems,
+     	infoElement:'',
+     	successElement:'',
+     	errorElement:'',
+     	warnElement:''
+    }
+  },
+  methods:{
+  	handleClick(e,index){
+  		if(index === 0){
+  			console.log('would be created an infoElement:',index)
+				infoItems.push(this.index)
+  		}else if(index === 1){
+  			console.log('would be created an errorElement:',index)
+				errorItems.push(this.index)
+  		}else if(index === 2){
+  			console.log('would be created a successElement:',index)
+				successItems.push(this.index)
+  		}else if(index === 3){
+  			console.log('would be created a warnElement:',index)
+				warnItems.push(this.index)
+  		}
+  	}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  height: 20px;
+}
+
+li {
+	display: inline-block;
+  float: left;
+  margin: 0 5px;
+  padding: 2px 5px;
+  border-radius: 4px;
+  border: 1px solid black;
+}
+li:hover{transition: all 0.5s;color:darkturquoise;border: 1px solid darkturquoise;cursor: pointer;}
+a {
+  color: #42b983;	
+}
+</style>
---
+++ src/components/info.vue
@@ -0,0 +1,69 @@
+<template>
+	<transition-group name = "slide">
+		<div class="parent" key = "i" :class="{empty:isEmpty}">
+			<div class="bro" ref = "bro">
+				<img src="../assets/info.png" alt="sorry to see this please wait" />
+				<div class="words">
+					<span class="close" @click = "removeElement">X</span>
+					<p class="titile">{{title}}</p>
+					<p class="content">{{content}}</p>
+				</div>
+			</div>			
+		</div>
+	</transition-group>
+</template>
+<script>
+	export default{
+		name : "info",
+		data(){
+			return{
+				isEmpty:false	
+			}
+		},
+		props : {
+			title : String,
+			content : String
+		},
+		methods:{
+			removeElement(){
+				console.log('remove this element')
+				const self = this
+				this.isEmpty = true
+				setTimeout(function(){
+//					alert('be going to remove')
+					self.$emit('remove')
+				},100)
+			}
+		},
+		mounted(){
+			console.log(window.getComputedStyle(this.$refs.bro).width)
+			const self = this
+			setTimeout(function(){
+				if(self.isEmpty){
+					return false
+				}else{
+					self.removeElement()				
+				}
+//				alert('i would be removed')
+			},3000)
+		}
+	}
+</script>
+<style scoped="scoped">
+.parent{border: 1px solid black;position: relative;width: 25%;margin-bottom: 10px;border-radius: 5px;left: 105%;animation: move .5s ease forwards;background-color: white;}
+@keyframes move{
+	from{left: 105%;}
+	to{left: 75%;}
+}
+.parent.empty{left:75%;animation: emptyHeight .5s linear forwards;}
+@keyframes emptyHeight{
+	100%{height: 100%;opacity: .5;}
+	50%{opacity: .5;}
+	0%{height: 0px;opacity: 0;}
+}
+img{width: 20%;position: absolute;top: 10px;}
+.words{overflow: hidden;}
+.words>span:hover{cursor: pointer;}
+.close{float: right;margin: 5px;}
+.titile,.content{margin-left: 46px;}
+</style>
